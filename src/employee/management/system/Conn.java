f4b66ddceca5f39db3091ebf3c3a7a8d6c84c4de
KajalSoni955 <kjlsoni37@gmail.com>
2021-01-31 01:50:45 +0530

    added database and making a connection to it

--- src/employee/management/system/Conn.java
+++ src/employee/management/system/Conn.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package employee.management.system;
+//to make a conncetion with the database
+//also added mysql-jdbc driver in libraries
 
+//import java.sql.DriverManager;
+import java.sql.*;
 /**
  *
  * @author Kajal C Soni
  */
 public class Conn {
+    
+     public Connection c;
+     public Statement s; //sql queries are the statements
+ 
+    public Conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employee-management-system","root","");
+            //c = DriverManager.getConnection("jdbc:mysql:///db name","db username","db pswrd");
+            s = c.createStatement();
+        }catch(Exception e) {
+           System.out.println(e);
+        }
+    }
     
 }
